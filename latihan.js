bb2c84612f9652b9ea42dea4557144bbeb80157d
Refri Rifwan <refririfwan@gmail.com>
2019-12-14 15:41:06 +0700

    POPUP BOX

--- latihan.js
+++ latihan.js
@@ -1,1 +1,11 @@
-alert('helo');
+alert('selamat  datang ...');
+var lagi = true;
+
+while (lagi) {
+    var nama = prompt('masukan nama : ');
+    alert('halo ' + nama);
+
+    lagi = confirm('coba lagi ?');
+}
+
+alert('terimakasih');
