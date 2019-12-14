1181d12103059ea79ae5a69b96d348ec2dbac1a9
Refri Rifwan <refririfwan@gmail.com>
2019-12-14 17:46:26 +0700

    pengulangan while

---
+++ while.js
@@ -0,0 +1,11 @@
+// var ulang = true;
+// while (ulang) {
+//     console.log('Hello  Wolrd');
+//     ulang = confirm('lagi ?');
+// }
+
+var nilaiAwal = 1;
+while (nilaiAwal <= 10) {
+    console.log('Angkot No. ' + nilaiAwal + ' beroperasi dengan baik.');
+    nilaiAwal++;
+}
