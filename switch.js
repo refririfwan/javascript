6d4dd67778b92b05c377899dc27062045a678154
Refri Rifwan <refririfwan@gmail.com>
2019-12-15 10:22:56 +0700

    switch case

---
+++ switch.js
@@ -0,0 +1,16 @@
+var angka = prompt('Masukan angka :');
+angka = parseInt(angka);
+switch (angka) {
+    case 1:
+        alert('anda memasukan angka ' + angka);
+        break;
+    case 2:
+        alert('anda memasukan angka ' + angka);
+        break;
+    case 3:
+        alert('anda memasukan angka ' + angka);
+        break;
+    default:
+        alert('angka yang anda masukan salah');
+        break;
+}
