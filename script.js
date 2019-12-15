87eaaa2c1acbcbaad88b249e927a708a6a596722
Refri Rifwan <refririfwan@gmail.com>
2019-12-15 10:39:47 +0700

    pengulangan bersarang

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-var x = 10;
-console.log('hello world');
-console.log('isi dari variable x adalah' + x);
+// var x = 10;
+// console.log('hello world');
+// console.log('isi dari variable x adalah' + x);
+
+var s = '';
+
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j <= i; j++) {
+        s += '*';
+    }
+    s += '\n';
+}
+console.log(s);
