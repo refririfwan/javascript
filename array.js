2deada22eacf2b401ed4ea308b8d8f747078ae9e
Refri Rifwan <refririfwan@gmail.com>
2019-12-17 09:26:23 +0700

    array foreach, map, & sort

--- array.js
+++ array.js
@@ -44,7 +44,31 @@
 
 // 5. slice (mengiris array)
 // slice(awal,akhir)
-var arr = ["refri", "rifwan", "daharmi", "rifwanda"];
-console.log(arr.join('\n'));
-var arr2 = arr.slice(0, 2);
-console.log(arr2.join('\n'));
+// var arr = ["refri", "rifwan", "daharmi", "rifwanda"];
+// console.log(arr.join('\n'));
+// var arr2 = arr.slice(0, 2);
+// console.log(arr2.join('\n'));
+
+// 6. foreach
+// var angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// angka.forEach(function (e) {
+//     console.log(e)
+// });
+// var nama = ["refri", "rifwan", "daharmi", "rifwanda"];
+// nama.forEach(function (e, i) {
+//     console.log('Mahasiswa ke-' + (i + 1) + ' adalah ' + e);
+// });
+
+// 7. map (mengembalikan array)
+// var angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var angka2 = angka.map(function (e) {
+//     return e * 2;
+// });
+// console.log(angka2.join('\n'));
+
+// 8. sort
+var angka = [1, 2, 3, 10, 4, 5, 0, 6, 7, 20, 8, 9];
+angka.sort(function (a, b) {
+    return a - b;
+});
+console.log(angka.join('\n'));
