33cfd9a2a196ef1232e72b0fb824d4fc3a1a004f
Refri Rifwan <refririfwan@gmail.com>
2019-12-17 14:49:48 +0700

    object

---
+++ object.js
@@ -0,0 +1,11 @@
+// membuat object
+var mhs = {
+    nama: "Refri Rifwan",
+    umur: 21,
+    ips: [3.00, 3.25, 3.50],
+    alamat: {
+        jalan: "JL. abc No. 123",
+        kota: "Malang",
+        provinsi: "Jawa Timur"
+    }
+};
