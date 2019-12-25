8e35c8543363eeee400343e9523ec7b8fbfb28ac
Refri Rifwan <refririfwan@gmail.com>
2019-12-25 16:06:05 +0700

    DOM Traversal

---
+++ JavascriptDOM/DOMTraversal/script.js
@@ -0,0 +1,22 @@
+// Event Handling
+// const close = document.getElementsByClassName('close')[0];
+// const card = document.getElementsByClassName('card')[0];
+
+// close.addEventListener('click', function () {
+//     card.style.display = 'none';
+// })
+
+// DOM Traversal
+// const close = document.getElementsByClassName('close');
+// for (let i = 0; i < close.length; i++) {
+//     close[i].addEventListener('click', function (event) {
+//         // close[i].parentElement.style.display = 'none';
+//         event.target.parentElement.style.display = 'none';
+//     });
+// }
+const close = document.querySelectorAll('.close');
+close.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+        e.target.parentElement.style.display = 'none';
+    });
+});
