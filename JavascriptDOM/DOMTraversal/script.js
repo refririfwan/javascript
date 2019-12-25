b4d34994f2f1c26d4c06f02dfb335cbe5baad57e
Refri Rifwan <refririfwan@gmail.com>
2019-12-25 16:34:16 +0700

    prevent default & bubbling

--- JavascriptDOM/DOMTraversal/script.js
+++ JavascriptDOM/DOMTraversal/script.js
@@ -18,5 +18,17 @@ const close = document.querySelectorAll('.close');
 close.forEach(function (el) {
     el.addEventListener('click', function (e) {
         e.target.parentElement.style.display = 'none';
+        // prevent default
+        e.preventDefault();
+        // menghentikan bubbling
+        e.stopPropagation();
     });
 });
+
+// event bubbling
+const cards = document.querySelectorAll('.card');
+cards.forEach(function (card) {
+    card.addEventListener('click', function (e) {
+        alert('ok');
+    })
+})
